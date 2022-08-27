d7b55a38622914f31c446373cc6cb4ec99537987
Anna Molodtsova <renasafetysea@mail.ru>
2022-08-28 01:07:40 +0300

    сделала ДЗ

---
+++ src/Autor.java
@@ -0,0 +1,11 @@
+public class Autor {
+     private String name;
+    public Autor(String name){
+        this.name =name;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Autor stivenKing = new Autor("Stiven King");
+        System.out.println("Имя Автора - " + stivenKing.getName());
+        Book later = new Book("Later", 2021);
+        System.out.println("Название книги -  " + later.getTitle());
+        System.out.println("Издана в - " + later.getYear()+ " году");
+        later.setYear(2022);
+        System.out.println("Издана в - " + later.getYear() + " году");
+        Autor jackLondon = new Autor("Jack London");
+        System.out.println("Имя автора - " + jackLondon.getName());
+        Book loveOfLife = new Book("Live of love", 1907);
+        System.out.println("Название книги - " + loveOfLife.getTitle());
+        System.out.println("Издана в - " + loveOfLife.getYear() + " году");
+        loveOfLife.setYear(1908);
+        System.out.println("Издана в - " + loveOfLife.getYear() + " году");
     }
 }
